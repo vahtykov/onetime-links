7de6d948fa61be9ae9c33dfe31879e2a2a556c97
Ilya Vahtykov <ilya.v@profi.io>
2024-06-19 00:57:35 +0300

    moved port to ENV

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
@@ -5,6 +7,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(2007);
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
